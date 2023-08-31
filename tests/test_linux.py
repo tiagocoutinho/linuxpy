0eb837f6ad0ffd1e931d235305a1b7a10164f7e8
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2023-08-31 22:40:05 +0200

    Preparing 0.0.1

---
+++ tests/test_linux.py
@@ -0,0 +1,14 @@
+#
+# This file is part of the python-linux project
+#
+# Copyright (c) 2023 Tiago Coutinho
+# Distributed under the GPLv3 license. See LICENSE for more info.
+
+"""Tests for `linux` package."""
+
+from ward import test
+
+
+@test("basic linux")
+def test_linux():
+    ...
