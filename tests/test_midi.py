9e9a2b8cacee1092be8eab157b4dad08efe00cbd
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2023-11-19 07:04:31 +0100

    Add midi tests

--- tests/test_midi.py
+++ tests/test_midi.py
@@ -2,10 +2,69 @@ import os
 import uuid
 from functools import cache
 
-from ward import skip, test
+from ward import raises, skip, test
 
-from linuxpy.midi.device import SEQUENCER_PATH, Sequencer, iter_read_clients
-from linuxpy.midi.raw import snd_seq_client_info
+from linuxpy.midi.device import (
+    QUEUE_DIRECT,
+    READ_WRITE,
+    SEQUENCER_PATH,
+    EventType,
+    MidiError,
+    Sequencer,
+    Version,
+    iter_read_clients,
+    read_port_info,
+    to_address,
+    to_event_type,
+)
+from linuxpy.midi.raw import ClientType, PortType, snd_seq_addr, snd_seq_client_info
+
+
+def assert_address(addr, client, port):
+    assert addr.client == client
+    assert addr.port == port
+
+
+for addr in ((4, 6), snd_seq_addr(4, 6)):
+
+    @test("to_address")
+    def _(addr=addr):
+        address = to_address(addr)
+        assert address.client == 4
+        assert address.port == 6
+
+
+for noteon in ("noteon", "note_on", "note on", EventType.NOTEON, 6):
+
+    @test("to_event_type")
+    def _(noteon=noteon):
+        assert to_event_type(noteon) == EventType.NOTEON
+
+
+@test("version")
+def _():
+    v = Version(4, 5, 6)
+    assert v.major == 4
+    assert v.minor == 5
+    assert v.patch == 6
+    assert int(v) == (4 << 16) + (5 << 8) + 6
+    assert repr(v) == "4.5.6"
+    assert v[0] == 4
+    assert v[1] == 5
+    assert v[2] == 6
+
+    assert Version.from_number((4 << 16) + (5 << 8) + 6) == v
+    assert Version.from_str("4.5.6") == v
+    assert Version(4, 5, 5) < v
+    assert Version(4, 5, 7) > v
+
+    with raises(ValueError) as error:
+        assert v > "hello"
+    assert "Comparison with non-Version object" in error.raised.args[0]
+
+    with raises(ValueError) as error:
+        assert v < 56.7
+    assert "Comparison with non-Version object" in error.raised.args[0]
 
 
 @cache
@@ -62,3 +121,201 @@ def _():
             assert not dev.closed
         assert not dev.closed
     assert dev.closed
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("system info")
+def _():
+    with Sequencer() as seq:
+        system_info = seq.system_info
+        assert system_info.queues > 0
+        assert system_info.clients > 0
+        assert system_info.ports > 0
+        assert system_info.channels > 0
+        assert system_info.cur_clients > 0
+        assert system_info.cur_queues >= 0
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("running mode")
+def _():
+    with Sequencer() as seq:
+        running_mode = seq.running_mode
+        assert running_mode.client == ClientType.SYSTEM
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("client info")
+def _():
+    with Sequencer() as seq:
+        client_info = seq.client_info
+        assert client_info.client == seq.client_id
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("port info")
+def _():
+    with Sequencer() as seq:
+        port = seq.create_port()
+        port_info = read_port_info(seq, seq.client_id, int(port))
+        assert_address(port_info.addr, port.client_id, port.port_id)
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("create port")
+def _():
+    with Sequencer() as remote, Sequencer() as local:
+        ports_before = local.ports
+        port = remote.create_port()
+        ports_after = local.ports
+        assert len(ports_after) == len(ports_before) + 1
+        assert port.type == PortType.MIDI_GENERIC | PortType.APPLICATION
+        assert READ_WRITE in port.capability
+        assert port.address.client == remote.client_id
+        assert "LocalPort" in repr(port)
+        assert "linuxpy port" in str(port)
+
+
+def assert_noteon_event(event, source_port, target_port):
+    assert event.type == EventType.NOTEON
+    assert event.client_id == event.event.source.client
+    assert event.port_id == event.event.source.port
+    assert event.source_client_id == source_port.client_id
+    assert event.source_port_id == source_port.port_id
+    assert event.dest_client_id == target_port.client_id
+    assert event.dest_port_id == target_port.port_id
+    assert event.source.client == source_port.client_id
+    assert event.source.port == source_port.port_id
+    assert event.dest.client == target_port.client_id
+    assert event.dest.port == target_port.port_id
+
+    assert event.queue == QUEUE_DIRECT
+    assert not event.is_variable_length_type
+    assert event.note.velocity == 44
+    assert event.note.channel == 0
+    assert f"{event.client_id}:{event.port_id}" in str(event)
+    assert "<Event type=NOTEON>" == repr(event)
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("delete port")
+def _():
+    with Sequencer() as seq:
+
+        def port_uids():
+            return {(p.client_id, p.port_id) for p in seq.ports}
+
+        port = seq.create_port()
+        uid = (port.client_id, port.port_id)
+        assert uid in port_uids()
+        port.delete()
+        assert uid not in port_uids()
+
+        port = seq.create_port()
+        uid = (port.client_id, port.port_id)
+        assert uid in port_uids()
+        seq.delete_port(int(port))
+        assert uid not in port_uids()
+
+        port = seq.create_port()
+        uid = (port.client_id, port.port_id)
+        assert uid in port_uids()
+        seq.delete_port(port)
+        assert uid not in port_uids()
+
+        with Sequencer() as remote:
+            port = remote.create_port()
+            with raises(MidiError):
+                seq.delete_port(port)
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("(dis)connect_from/to")
+def _():
+    with Sequencer() as remote, Sequencer() as local:
+        remote_port = remote.create_port()
+        local_port = local.create_port()
+
+        addr = remote_port.client_id, remote_port.port_id
+        uid = remote_port.client_id, remote_port.port_id, local_port.client_id, local_port.port_id
+        local_port.connect_from(*addr)
+        assert uid in local.subscriptions
+        local_port.disconnect_from(*addr)
+        assert uid not in local.subscriptions
+
+        uid = local_port.client_id, local_port.port_id, remote_port.client_id, remote_port.port_id
+        local_port.connect_to(*addr)
+        assert uid in local.subscriptions
+        local_port.disconnect_to(*addr)
+        assert uid not in local.subscriptions
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("(dis)connect_from/to and delete error on non local")
+def _():
+    with Sequencer() as remote, Sequencer() as local:
+        remote_port = remote.create_port()
+
+        for port in local.ports:
+            if port.client_id == remote_port.client_id and port.port_id == remote_port.port_id:
+                break
+
+        with raises(MidiError) as error:
+            port.connect_from(0, 1)
+        assert "Can only connect local port" == error.raised.args[0]
+
+        with raises(MidiError) as error:
+            port.connect_to(0, 1)
+        assert "Can only connect local port" == error.raised.args[0]
+
+        with raises(MidiError) as error:
+            port.disconnect_from(0, 1)
+        assert "Can only disconnect local port" == error.raised.args[0]
+
+        with raises(MidiError) as error:
+            port.disconnect_to(0, 1)
+        assert "Can only disconnect local port" == error.raised.args[0]
+
+        with raises(MidiError) as error:
+            port.delete()
+        assert "Can only delete local port" == error.raised.args[0]
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("connect_from events")
+def _():
+    with Sequencer() as remote, Sequencer() as local:
+        remote_port = remote.create_port()
+        local_port = local.create_port()
+
+        local_port.connect_from(remote_port.client_id, remote_port.port_id)
+
+        remote_port.send("note on", velocity=44)
+
+        # Use a for loop with a break to make sure the Sequencer.__iter__ is closed
+        # using something like next(iter(seq)) wouldn't make the tests fail but if
+        # Sequencer.__iter__ needs a cleanup it would only get called when the seq
+        # object gets garbage collectecd
+        for event in local:
+            assert_noteon_event(event, remote_port, local_port)
+            break
+
+
+@skip(when=not is_sequencer_available(), reason="MIDI sequencer is not prepared")
+@test("async connect_from events")
+async def _():
+    with Sequencer() as remote, Sequencer() as local:
+        remote_port = remote.create_port()
+        local_port = local.create_port()
+
+        local_port.connect_from(remote_port.client_id, remote_port.port_id)
+
+        remote_port.send("note on", velocity=44)
+
+        # Use a for loop with a break to make sure the Sequencer.__aiter__ is closed
+        # using something like next(iter(seq)) wouldn't make the tests fail but if
+        # Sequencer.__aiter__ needs a cleanup it would only get called when the seq
+        # object gets garbage collectecd
+        async for event in local:
+            assert_noteon_event(event, remote_port, local_port)
+            break
