7753a51173d8978a381e63b24d18321b7d7ec124
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2023-12-21 00:06:44 +0100

    Add utils tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -6,7 +6,23 @@
 
 from ward import raises, test
 
-from linuxpy.util import Version
+from linuxpy.util import Version, bcd_version, bcd_version_tuple, make_find
+
+for bcd, expected in [
+    (0x3, (3,)),
+    (0x2549, (25, 49)),
+    (0x999897, (99, 98, 97)),
+    (0x123, (1, 23)),
+    (0x12345, (1, 23, 45)),
+]:
+
+    @test("bcd version tuple")
+    def _(bcd=bcd, expected=expected):
+        assert bcd_version_tuple(bcd) == expected
+
+    @test("bcd version")
+    def _(bcd=bcd, expected=expected):
+        assert bcd_version(bcd) == ".".join(map(str, expected))
 
 
 @test("version")
@@ -25,6 +41,8 @@ def _():
     assert Version.from_str("4.5.6") == v
     assert Version(4, 5, 5) < v
     assert Version(4, 5, 7) > v
+    assert (4, 5, 7) > v
+    assert (4, 5, 5) < v
 
     with raises(ValueError) as error:
         assert v > "hello"
@@ -32,3 +50,32 @@ def _():
     with raises(ValueError) as error:
         assert v < 56.7
     assert "Comparison with non-Version object" in error.raised.args[0]
+
+
+@test("make find")
+def _():
+    class Device:
+        def __init__(self, i):
+            self.i = i
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, *args):
+            return
+
+    devices = [Device(a) for a in range(3)]
+
+    def i():
+        return iter(devices)
+
+    find = make_find(i)
+
+    assert find() is devices[0]
+    assert find(i=1) is devices[1]
+    assert find(custom_match=lambda device: device.i > 0) is devices[1]
+    assert find(custom_match=lambda device: device.i > 2) is None
+    assert len(tuple(find(find_all=True))) == 3
+    assert all(a is b for a, b in zip(devices, find(find_all=True)))
+    assert all(a is b for a, b in zip(devices[1:2], find(find_all=True, i=1)))
+    assert all(a is b for a, b in zip(devices[1:3], find(find_all=True, custom_match=lambda device: device.i > 0)))
