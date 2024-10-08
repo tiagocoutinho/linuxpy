a325aacc6b42c1dda5d5e9111a92d46b729cf17c
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2024-10-08 20:42:04 +0200

    Look for gpio sim in given order; Remove unused gpio-sim code

--- linuxpy/gpio/sim.py
+++ linuxpy/gpio/sim.py
@@ -28,7 +28,7 @@ from pathlib import Path
 
 from linuxpy.configfs import CONFIGFS_PATH
 from linuxpy.gpio.device import get_chip_info
-from linuxpy.types import Optional, Union
+from linuxpy.types import Optional
 
 GPIOSIM_PATH: Optional[Path] = None if CONFIGFS_PATH is None else CONFIGFS_PATH / "gpio-sim"
 
@@ -39,50 +39,9 @@ def find_gpio_sim_file(num_lines=None) -> Optional[Path]:
     Returns:
         _type_: _description_
     """
-    for path in sorted(Path("/dev").glob("gpiochip*"))[::-1]:
+    for path in sorted(Path("/dev").glob("gpiochip*")):
         with path.open("rb") as fobj:
             info = get_chip_info(fobj)
             if "gpio-sim" in info.label:
                 if num_lines is None or info.lines == num_lines:
                     return path
-
-
-class GPIOSim:
-    def __init__(self, name: str):
-        self.name = name
-        self.path = GPIOSIM_PATH / self.name
-        self.banks = [0]
-
-    def __enter__(self):
-        self.configure()
-        self.enable()
-
-    def __exit__(self):
-        self.disable()
-        self.destroy()
-
-    def write(self, fname, value):
-        with open(self.path / fname, "wb") as fobj:
-            fobj.write(str(value))
-
-    def create_bank(self):
-        self.path.mkdir(exist_ok=True)
-
-    def configure(self):
-        self.path.mkdir(exist_ok=True)
-        for bank in self.banks:
-            bname = f"gpio-bank{bank}"
-            path = self.path / bname
-            path.mkdir(exist_ok=True, mode=0o770)
-            self.write(path / "num_lines", 16)
-
-    def destroy(self): ...
-
-    def live(self, value: Union[bool, int]):
-        self.write("live", 1 if value else 0)
-
-    def enable(self):
-        self.live(1)
-
-    def disable(self):
-        self.live(0)
