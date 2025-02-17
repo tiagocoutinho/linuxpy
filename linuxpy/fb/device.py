b0263283f6a471963005b73d84916054c0fbc956
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2025-02-17 07:04:50 +0100

    WIP

--- linuxpy/fb/device.py
+++ linuxpy/fb/device.py
@@ -60,17 +60,24 @@ def get_raw_var_screen_info(fd) -> raw.fb_var_screeninfo:
     return ioctl(fd, raw.IOC.GET_VSCREENINFO, raw.fb_var_screeninfo())
 
 
+def get_raw_colormap(fd):
+    return ioctl(fd, raw.IOC.GETCMAP, raw.fb_cmap())
+
+
 class Device(BaseDevice):
     PREFIX = "/dev/fb"
 
+    def get_fix_screen_info(self):
+        return get_fix_screen_info(self)
+
 
 def iter_files(path: PathLike = "/dev") -> Iterable[Path]:
-    """Returns an iterator over all fb files"""
+    """Returns an iterator over all frame buffer files"""
     return iter_device_files(path=path, pattern="fb*")
 
 
 def iter_devices(path: PathLike = "/dev", **kwargs) -> Iterable[Device]:
-    """Returns an iterator over all fb devices"""
+    """Returns an iterator over all frame buffer devices"""
     return (Device(name, **kwargs) for name in iter_files(path=path))
 
 
