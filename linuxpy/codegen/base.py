8cf11ffdae481a118baff04c01e2b96af7ae456f
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2025-05-23 16:14:42 +0200

    codegen: keep order between struct and union

--- linuxpy/codegen/base.py
+++ linuxpy/codegen/base.py
@@ -219,12 +219,12 @@ def find_xml_base_type(etree, context, type_id):
 def get_structs(header_filename, xml_filename, decode_name):
     etree = xml.etree.ElementTree.parse(xml_filename)
     header_tag = etree.find(f"File[@name='{header_filename}']")
-    structs = {}
+    struct_map = {}
+    structs = []
     if header_tag is None:
         return structs
     header_id = header_tag.get("id")
-    nodes = etree.findall(f"Struct[@file='{header_id}']")
-    nodes += etree.findall(f"Union[@file='{header_id}']")
+    nodes = [node for node in etree.findall(f"*[@file='{header_id}']") if node.tag in {"Union", "Struct"}]
     for node in nodes:
         members = node.get("members")
         if members is not None:
@@ -243,10 +243,11 @@ def get_structs(header_filename, xml_filename, decode_name):
         if name:
             name = decode_name(name)
         struct = CStruct(node, name, fields, pack)
-        structs[struct.id] = struct
-    for struct in structs.values():
+        struct_map[struct.id] = struct
+        structs.append(struct)
+    for struct in structs:
         if struct.context_id != "_1":
-            parent = structs.get(struct.context_id)
+            parent = struct_map.get(struct.context_id)
             if parent:
                 parent.children[struct.id] = struct
                 struct.parent = parent
@@ -254,7 +255,7 @@ def get_structs(header_filename, xml_filename, decode_name):
                 logging.error("Could not find parent")
         if not struct.name and struct.parent:
             struct.name = f"M{len(struct.parent.children)}"
-    for struct in structs.values():
+    for struct in structs:
         for node in struct.node_members:
             name = node.get("name")
             base = find_xml_base_type(etree, struct, node.get("type"))
@@ -352,8 +353,7 @@ def run(name, headers, template, macro_enums, output=None, decode_struct_name=nu
         xml_filename = os.path.join(temp_dir, base_header + ".xml")
         cmd = f"castxml --castxml-output=1.0.0 -o {xml_filename} {header}"
         assert os.system(cmd) == 0
-        new_structs = get_structs(header, xml_filename, decode_name=decode_struct_name)
-        structs += list(new_structs.values())
+        structs += get_structs(header, xml_filename, decode_name=decode_struct_name)
 
         get_enums(header, xml_filename, macro_enums, decode_name=decode_enum_name)
 
