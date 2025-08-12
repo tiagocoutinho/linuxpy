80c4687081e0e4447e0d406783460208e4f31e82
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2025-08-12 18:14:12 +0200

    Fix codegen

--- linuxpy/codegen/cli.py
+++ linuxpy/codegen/cli.py
@@ -4,7 +4,6 @@ from .gpio import main as gpio_main
 from .input import main as input_run
 from .magic import main as magic_run
 from .midi import main as midi_run
-from .network import main as network_run
 from .usbfs import main as usbfs_run
 from .usbids import main as usbids_main
 from .video import main as video_main
@@ -17,7 +16,6 @@ def main():
     input_run()
     magic_run()
     midi_run()
-    network_run()
     usbfs_run()
     usbids_main()
     video_main()
