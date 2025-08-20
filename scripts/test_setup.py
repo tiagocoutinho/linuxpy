e7ac3f965ad820997a5bdccb7ca39f3764247f68
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2025-08-20 15:06:10 +0200

    Try to add midi

--- scripts/test_setup.py
+++ scripts/test_setup.py
@@ -24,6 +24,7 @@ MODULES = {
     "uleds": "",
     "gpio-sim": "",
     "vivid": "n_devs=1 node_types=0xe1d3d vid_cap_nr=190 vid_out_nr=191 meta_cap_nr=192 meta_out_nr=193",
+    "snd_seq_midi": "",
 }
 
 
